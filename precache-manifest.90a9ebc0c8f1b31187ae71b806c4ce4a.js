4c688b0b4f1c43a9cd6b4704033634050779f92f
lakshayman <lakshaymanchanda73@gmail.com>
2020-04-16 02:54:35 +0530

    Updates

---
+++ precache-manifest.90a9ebc0c8f1b31187ae71b806c4ce4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38fc2f2ef4fc6d05001c6874dab2a6a",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "984197435c1e9401e6f0",
+    "url": "/tictactoe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "38c58a404a3fb46dd5f4",
+    "url": "/tictactoe/static/js/2.b102e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.b102e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984197435c1e9401e6f0",
+    "url": "/tictactoe/static/js/main.70686418.chunk.js"
+  },
+  {
+    "revision": "0066fcfcd7c5702b5829",
+    "url": "/tictactoe/static/js/runtime-main.4655614b.js"
+  }
+]);
